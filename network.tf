229e07118cee5644174b57757e6c7f81bad6f1f6
root <laxmitha.pai@niveussolutionse.com>
2022-09-26 12:07:27 +0000

    Added network module2

--- network.tf
+++ network.tf
@@ -2,6 +2,6 @@ module "network" {
   source  = "app.terraform.io/tf-testlab/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   
 }
